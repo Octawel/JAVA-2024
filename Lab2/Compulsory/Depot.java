363d0b95080d87dd0711556b4d1d57f785198693
Octawel <95048266+Octawel@users.noreply.github.com>
2024-03-08 18:30:04 +0200

    Add files via upload

---
+++ Lab2/Compulsory/Depot.java
@@ -0,0 +1,22 @@
+package Compulsory;
+
+class Depot {
+    private String name;
+
+    public Depot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Depot{name='" + name + '\'' + '}';
+    }
+}
