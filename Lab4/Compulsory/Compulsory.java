85c52260de5460c99029fa94312a145613ed7de2
Octawel <95048266+Octawel@users.noreply.github.com>
2024-03-19 19:06:11 +0200

    Add files via upload

--- Lab4/Compulsory/Compulsory.java
+++ Lab4/Compulsory/Compulsory.java
@@ -1,16 +1,13 @@
 package Compulsory;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
+import com.github.javafaker.Faker;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Compulsory {
     public static void main(String[] args) {
-        List<Person> persons = generateRandomPersons(20); // Generate 20 random persons
-
+        List<Person> persons = generateRandomPersons(20);
         LinkedList<Driver> drivers = persons.stream()
                 .filter(p -> p instanceof Driver)
                 .map(p -> (Driver) p)
@@ -22,23 +19,30 @@ public class Compulsory {
                 .map(p -> (Passenger) p)
                 .collect(Collectors.toCollection(() -> new TreeSet<>(java.util.Comparator.comparing(Passenger::getName))));
 
-        // Print Drivers
         System.out.println("Drivers sorted by age:");
         drivers.forEach(d -> System.out.println(d.getName() + ", " + d.getAge()));
 
-        // Print Passengers
         System.out.println("\nPassengers sorted by name:");
         passengers.forEach(p -> System.out.println(p.getName() + ", " + p.getAge()));
     }
 
     private static List<Person> generateRandomPersons(int numberOfPersons) {
         List<Person> persons = new LinkedList<>();
+        Set<String> usedNames = new HashSet<>();
+        Faker faker = new Faker();
         Random random = new Random();
-        for (int i = 0; i < numberOfPersons; i++) {
-            if (random.nextBoolean()) {
-                persons.add(new Driver("Driver" + i, 18 + random.nextInt(50), "Destination" + random.nextInt(5)));
-            } else {
-                persons.add(new Passenger("Passenger" + i, 18 + random.nextInt(50), "Destination" + random.nextInt(5)));
+
+        while (persons.size() < numberOfPersons) {
+            String name = faker.name().fullName();
+            if (!usedNames.contains(name)) {
+                usedNames.add(name);
+                int age = 18 + random.nextInt(50);
+                String destination = "Destination" + random.nextInt(5);
+                if (random.nextBoolean()) {
+                    persons.add(new Driver(name, age, destination));
+                } else {
+                    persons.add(new Passenger(name, age, destination));
+                }
             }
         }
         return persons;
