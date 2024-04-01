dd696cf7bfd9daa5933b4514dc591d519127929d
Octawel <95048266+Octawel@users.noreply.github.com>
2024-04-01 21:11:03 +0300

    Add files via upload

---
+++ Lab6/Compulsory/Compulsory.java
@@ -0,0 +1,9 @@
+package Compulsory;
+
+import javax.swing.SwingUtilities;
+
+public class Compulsory {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(GameFrame::new);
+    }
+}
