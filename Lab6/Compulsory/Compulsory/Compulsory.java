35c363b851a8e6622c8bc5bfad94334906813520
Octawel <95048266+Octawel@users.noreply.github.com>
2024-04-01 21:10:43 +0300

    Delete Lab6/Compulsory/Compulsory directory

--- Lab6/Compulsory/Compulsory/Compulsory.java
+++
@@ -1,9 +0,0 @@
-package Compulsory;
-
-import javax.swing.SwingUtilities;
-
-public class Compulsory {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(GameFrame::new);
-    }
-}
