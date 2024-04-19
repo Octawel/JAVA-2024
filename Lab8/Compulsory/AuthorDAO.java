6bdbdcc905cd04bddbce12df7a5d6072046c50b5
Octawel <95048266+Octawel@users.noreply.github.com>
2024-04-20 01:54:23 +0300

    Add files via upload

--- Lab8/Compulsory/AuthorDAO.java
+++ Lab8/Compulsory/AuthorDAO.java
@@ -13,7 +13,7 @@ public class AuthorDAO {
     }
 
     public void addAuthor(String name, String country) throws SQLException {
-        String query = "INSERT INTO Authors (name, country) VALUES (?, ?)";
+        String query = "INSERT INTO Authors (name, country) VALUES (?, ?) ON DUPLICATE KEY UPDATE name=name, country=country";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, name);
             statement.setString(2, country);
@@ -22,13 +22,14 @@ public class AuthorDAO {
     }
 
     public void listAuthors() throws SQLException {
-        String query = "SELECT * FROM Authors";
+        String query = "SELECT * FROM Authors ORDER BY author_id";  // Added ORDER BY clause here
         try (PreparedStatement statement = connection.prepareStatement(query);
              ResultSet resultSet = statement.executeQuery()) {
             while (resultSet.next()) {
-                System.out.println("ID: " + resultSet.getInt("author_id") + ", Name: " + resultSet.getString("name") + ", Country: " + resultSet.getString("country"));
+                System.out.println("ID: " + resultSet.getInt("author_id") +
+                        ", Name: " + resultSet.getString("name") +
+                        ", Country: " + resultSet.getString("country"));
             }
         }
     }
 }
-
